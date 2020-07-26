54b2672f3692117104bc9e7ca85b2a162af9c26b
Nicholas Klaene <nicholasklaene@gmail.com>
2020-07-26 19:21:15 -0400

    create slide show for about.html

--- app.js
+++ app.js
@@ -35,7 +35,9 @@ let navHTML = `
         </div>
 `;
 
-/* Functions */
+// Functions
+
+//Utility function to make an html element
 htmlToElement = html => {
     let template = document.createElement('template');
     html = html.trim();
@@ -43,6 +45,33 @@ htmlToElement = html => {
     return template.content.firstChild;
 };
 
+// Slide Show (about.html)
+plusSlides = n => {
+  showSlides(slideIndex += n);
+}
+
+currentSlide = n => {
+  showSlides(slideIndex = n);
+}
+
+showSlides = n => {
+  let slides = document.getElementsByClassName('slides');
+  let dots = document.getElementsByClassName('dot');
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+  }
+  for (let i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex-1].style.display = 'block';
+  dots[slideIndex-1].className += ' active';
+}
+
 let navTemplate = htmlToElement(navHTML);
 
 document.querySelector('.first').prepend(navTemplate);
+
+let slideIndex = 1;
+showSlides(slideIndex);
