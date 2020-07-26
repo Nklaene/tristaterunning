85983b384bc188faf630733f48954f986c2c0b4b
Nicholas Klaene <nicholasklaene@gmail.com>
2020-07-26 18:57:47 -0400

    created about.html and about.css

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
 // Variables + templates 
-let navHTML = `<div class="nav-bar">
+let navHTML = `
+        <div class="nav-bar">
             <ul class="nav-items">
-                <li class="nav-item logo"><img src="./images/logo.png" alt=""></li>
-                <li class="nav-item"><a href="index.html">Home</a></li>
-                <li class="nav-item"><a href="about.html">About</a></li>
-                <li class="nav-item"><a href="events.html">Events</a></li>
-                <li class="nav-item"><a href="fittings.html">Fittings</a></li>
-                <li class="nav-item"><a href="contact.html">Contact</a></li>
-                <li class="nav-item"><a href="training.html">Training</a></li>
-                <li class="nav-item"><a href="wellness.html">Wellness</a></li>
-                <li class="nav-item"><a href="shop.html">Shop</a></li>
+                <li class="nav-item logo">
+                    <a href="index.html">
+                        <img src="./images/logo.png" alt="logo">
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="about.html">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="events.html">Events</a>
+                </li>
+                <li class="nav-item">
+                    <a href="fittings.html">Fittings</a>
+                </li>
+                <li class="nav-item">
+                    <a href="contact.html">Contact</a>
+                </li>
+                <li class="nav-item">
+                    <a href="training.html">Training</a>
+                </li>
+                <li class="nav-item">
+                    <a href="wellness.html">Wellness</a>
+                </li>
+                <li class="nav-item">
+                    <a href="shop.html">Shop</a>
+                </li>
             </ul>
-        </div>`
-
-let navTemplate = htmlToElement(navHTML);
+        </div>
+`;
 
 /* Functions */
-
-function htmlToElement(html) {
+htmlToElement = html => {
     let template = document.createElement('template');
     html = html.trim();
     template.innerHTML = html;
     return template.content.firstChild;
-}
+};
 
-
+let navTemplate = htmlToElement(navHTML);
 
 document.querySelector('.first').prepend(navTemplate);
