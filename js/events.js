11dc5bc6158e819e32b5660120041688b91912b4
Nicholas Klaene <nicholasklaene@gmail.com>
2020-07-28 13:03:13 -0400

    created race class

---
+++ js/events.js
@@ -0,0 +1,9 @@
+class Race {
+    constructor(location, url, size, date, distance) {
+        this.location = location;
+        this.url = url;
+        this.size = size;
+        this.date = date;
+        this.distance = distance;
+    }
+}
